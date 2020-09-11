0ade514bef2f8bcc894b51873fb4a855b80a4801
Callum S <gamedevcallum@gmail.com>
2020-09-11 19:58:15 +0100

    Basic renderer, renders a square to screen.

--- src/engineTest/MainGameLoop.java
+++ src/engineTest/MainGameLoop.java
@@ -2,6 +2,9 @@ package engineTest;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop
 {
@@ -10,12 +13,33 @@ public class MainGameLoop
         DisplayManager.CreateDisplay(1280, 720, "Main Game");
         DisplayManager.SetFPS(30);
 
+        Loader loader = new Loader();
+
+        Renderer renderer = new Renderer();
+
+        float[] vertices = {
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0,
+
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f
+
+        };
+
+        RawModel model = loader.loadToAVO(vertices);
 
         while (!Display.isCloseRequested())
         {
+            renderer.prepare();
+
+            renderer.render(model);
+
             DisplayManager.UpdateDisplay();
         }
 
+        loader.cleanUp();
         DisplayManager.CloseDisplay();
     }
 }
